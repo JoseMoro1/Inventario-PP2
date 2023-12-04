ee69e319da000a36f7ebeed6104eafd24b9aa127
Jose Moro <142544817+JoseMoro1@users.noreply.github.com>
2023-12-04 18:21:47 -0300

    Add files via upload

--- inventario/navbar/navbar.php
+++ inventario/navbar/navbar.php
@@ -7,15 +7,25 @@
     <link rel="stylesheet" href="../navbar/stylenavbar.css">
 </head>
 <body>
-    
     <nav>
         <input type="checkbox" id="toogle">
+        <!-- <label for="tooggle"><img src="../images/logo mak tools.jpg" alt="menu"></label> -->
         <div class="logo">Inventario</div>
         <ul class="list">
             <li><a href="#">Inicio</a></li>
             <li><a href="../productos/productos.php">Productos</a></li>
-            <li><a href="#">Categorias</a></li>
-            <li><a href="#">Inventario</a></li>
+            <li><a href="#">Categorias</a>
+            <ul class="menu-vertical">
+                <li><a href="#">Matafuegos</a></li>
+                <li><a href="#">Botiquines</a></li>
+                <li><a href="#">Seguridad Vehicular</a></li>
+                <li><a href="#">Elementos de seguridad industrial</a></li>
+                <li><a href="#">Herramientas neumaticas</a></li>
+                <li><a href="#">Accesorios para gomeria y taller</a></li>
+                <li><a href="#">Productos para lubricentros</a></li>
+                <li><a href="#">Carritos porta Herramientas</a></li>
+            </ul>
+            </li>
             <li><a href="#">Salir</a></li>
         </ul>
 
@@ -24,8 +34,7 @@
             <div class="line"></div>
             <div class="line"></div>
         </label>
-
+        <!-- <a href=""><img src="../images/logo mak tools.jpg" alt=""></a> -->
     </nav>
-    
 </body>
 </html>
